d9b96a76a412ff2ea04aaefd5e9ada38a27e17df
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2026-04-29 16:34:32 -0300

    refactor: integra lista de hábitos ao Dashboard usando store global

--- components/HabitItem.tsx
+++ components/HabitItem.tsx
@@ -1,18 +1,15 @@
 import { View, Text, Pressable } from 'react-native'
 import { Circle, CircleCheck } from 'lucide-react-native'
-import { useState } from 'react'
+import useHabitStore from '../stores/useHabitStore'
+import { Habit } from '../types/habit'
 
-type HabitItemProps = {
-  habit: string
-  completed: boolean
-  time?: string
-}
-
-export function HabitItem({ habit, completed, time }: HabitItemProps) {
-  const [isCompleted, setIsCompleted] = useState(completed)
+export function HabitItem({ id, name, completed, time }: Habit) {
+  const toggleHabitCompleted = useHabitStore((state) => {
+    return state.toggleHabitCompleted
+  })
 
   function handleCheckboxPress() {
-    setIsCompleted((prev) => !prev)
+    toggleHabitCompleted(id)
   }
 
   return (
@@ -29,17 +26,17 @@ export function HabitItem({ habit, completed, time }: HabitItemProps) {
 
         <Text
           className={`text-xl font-inter-bold
-            ${isCompleted ? 'line-through text-secondary' : 'text-black'}`}
+            ${completed ? 'line-through text-secondary' : 'text-black'}`}
         >
-          {habit}
+          {name}
         </Text>
       </View>
 
       <View
         className={`w-16 h-16 items-center justify-center rounded-xl border-solid border-2
-        ${isCompleted ? 'bg-primary border-primary' : 'bg-white border-secondary'}`}
+        ${completed ? 'bg-primary border-primary' : 'bg-white border-secondary'}`}
       >
-        {isCompleted ? (
+        {completed ? (
           <CircleCheck size={24} color="white" />
         ) : (
           <Circle size={24} color="#777777" />
--- screens/DashboardScreen.tsx
+++ screens/DashboardScreen.tsx
@@ -1,13 +1,13 @@
 import { Header } from '../components/Header'
 import { DayOverview } from '../components/DayOverview'
-import { HabitItem } from '../components/HabitItem'
+import { HabitList } from '@/components/HabitList'
 
 export function DashboardScreen() {
   return (
     <>
       <Header />
       <DayOverview />
-      <HabitItem habit="Corrida matinal" completed={false} time="9:00 AM" />
+      <HabitList />
     </>
   )
 }
