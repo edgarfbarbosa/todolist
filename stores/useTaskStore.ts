9e0e93c081b8e13d986d38871e654ce6377a69d1
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2026-05-11 18:10:05 -0300

    feat: adiciona deleteTask para excluir tarefas

--- stores/useTaskStore.ts
+++ stores/useTaskStore.ts
@@ -5,25 +5,27 @@ interface TaskStore {
   tasks: Task[]
   toggleTaskCompleted: (id: string) => void
   createTask: (task: Task) => void
+  deleteTask: (id: string) => void
 }
 
 const taskStore: StateCreator<TaskStore> = (set) => ({
   tasks: [
     {
-      id: '1',
-      name: 'Definir prioridades do dia',
+      id: '3',
+      name: 'Desenvolver meu projeto',
       completed: false,
-      time: '9:00',
-    },
-    {
-      id: '2',
-      name: 'Revisar lista de tarefas',
-      completed: false,
+      pomodoros: 3,
+      time: '18:00 PM',
     },
   ],
 
   createTask: (task: Task) =>
     set((state) => ({ tasks: [...state.tasks, task] })),
+
+  deleteTask: (id: string) =>
+    set((state) => ({
+      tasks: state.tasks.filter((task) => task.id !== id),
+    })),
 
   toggleTaskCompleted: (id: string) =>
     set((state) => ({
